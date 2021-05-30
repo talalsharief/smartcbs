c7042ad1df6f75d80c10f722160e8d526e14d307
Aleena <aleenaysheikh22@gmail.com>
2021-05-31 02:30:13 +0500

    UI points

--- src/app/components/cp-add-meter-reading/cp-add-meter-reading.component.ts
+++ src/app/components/cp-add-meter-reading/cp-add-meter-reading.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-cp-add-meter-reading',
@@ -7,12 +8,17 @@ import { Keyboard } from '@ionic-native/keyboard/ngx';
   styleUrls: ['./cp-add-meter-reading.component.scss'],
 })
 export class CpAddMeterReadingComponent implements OnInit {
+  @ViewChild('sList') sList : IonList;
+
   isKeyboardHide=true;
+
   constructor(public keyboard:Keyboard) { 
     this.isKeyboardHide=true;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // this.sList.closeSlidingItems()
+  }
   ionViewWillEnter() {
     this.keyboard.onKeyboardWillShow().subscribe(()=>{
       this.isKeyboardHide=false;
